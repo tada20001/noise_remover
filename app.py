1f210a9e2fd712a3aaab30e66b28592dc13a25f9
tada20001 <tada20001@gmail.com>
2022-02-12 19:36:10 +0900

    6th commit

--- app.py
+++ app.py
@@ -17,14 +17,12 @@ expander_bar1.markdown("""
 st.sidebar.header('User Input Features')
 
 # 0. 파일 열기
-# Collects user input features into dataframe
 uploaded_file = st.sidebar.file_uploader("Upload your excel file", type=["xlsx"])
 try: 
     input_df = pd.read_excel(uploaded_file)
     for name in input_df.columns:
         if "Unnamed" in name:
             del input_df[name]
-
 except:
     input_df = None
 
@@ -33,7 +31,6 @@ st.sidebar.write("""***""")
 st.sidebar.subheader('유사과제 갯수 설정')
 rank = st.sidebar.text_input('과제수', 100)
 st.sidebar.write("""***""")
-
 st.sidebar.write("""### 과제명 입력
 * 과제간 유사도를 구하기 위해 기준이 될 과제명을 설정해야 됩니다.
 """)
@@ -51,17 +48,14 @@ if input_df is not None:
     input_df['연구내용'] = input_df['연구내용'].astype('str')
     tfidf = TfidfVectorizer(stop_words=stop_words_new)
     tfidf_matrix_content = tfidf.fit_transform(input_df['연구내용'])
-
-if input_df is not None:
     st.write("입력데이터 과제수는 ", tfidf_matrix_content.shape[0], "개입니다.")
 
-### 유사도 행렬
-if input_df is not None:
+    ### 2-1. 유사도 행렬
     cosine_sim_content = cosine_similarity(tfidf_matrix_content, tfidf_matrix_content)
     ### 과제명을 key, 과제 인텍스를 value로 하는 딕셔너리 만들기
     title_to_index = dict(zip(input_df['과제명'], input_df.index))
 
-        ## 추천시스템 함수
+    ## 2-2. 추천시스템 함수
     @st.cache
     def get_recommdataions1(title, rank, cosine_sim=cosine_sim_content):
         idx = title_to_index[title]
@@ -75,6 +69,7 @@ if input_df is not None:
         
         return input_df, input_df['과제명'].iloc[project_indices]
 
+    ## 2-3. 파일 다운로드 함수
     # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
     def filedownload(df):
         csv = df.to_csv(index=False, encoding='utf-8-sig')
@@ -82,14 +77,13 @@ if input_df is not None:
         href = f'<a href="data:file/csv;base64,{b64}" download="similar_projects.csv">Download CSV File</a>'
         return href
 
+    ## 2-4. 결과 출력
     df, titles = get_recommdataions1(title, int(rank))
-    st.markdown("""
-    #### 1. 유사과제 상위 10개 과제명
-    나머지는 파일을 다운로드해서 확인해 보세요!!
-    """)
+    st.markdown("""#### 1. 유사과제 상위 10개 과제명
+    나머지는 파일을 다운로드해서 확인해 보세요!!""")
     st.write(titles.values[:10])
 
-    # 5. Download dataframe
+    # Download dataframe
     st.write("""---""")
     st.write("""#### 2. 파일 다운로드""")
     st.write("""유사도 정보를 추가한 파일 다운로드(필드명 : sim_score). 
